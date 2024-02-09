12a9df0a413ba0aca6a2614fe7060feef21f627d
Kayki Letieri <kaykiletieri37@gmail.com>
2024-02-09 16:08:32 -0300

    feat: add swagger docs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s =>
+    s.SwaggerDoc("v1", new()
+    {
+        Title = "Desafio Backend GAC",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Kayki Letieri",
+            Email = "kaykiletieri37@gmail.com"
+        }
+    }));
 
 var app = builder.Build();
 
